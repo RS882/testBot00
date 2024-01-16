6af1ac9fd12b155e23a7462231da89b69ca2a13d
Iryna Yakymchuk <iryna.work.it@gmail.com>
2024-01-16 16:29:13 +0100

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("world2");
         System.out.println("world2");
         System.out.println("Hi, IgorV is here");
+        System.out.println("Hello World!");
     }
 }
