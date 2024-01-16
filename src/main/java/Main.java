f666ab4c7d0abaaa9ccc92b8557bda0e028d1b28
Iryna Yakymchuk <iryna.work.it@gmail.com>
2024-01-16 20:04:55 +0100

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,5 +6,8 @@ public class Main {
         System.out.println("world2");
         System.out.println("world2");
         System.out.println("world4");
+        System.out.println("world4");
+        System.out.println("world4");
+        System.out.println("world4");
     }
 }
